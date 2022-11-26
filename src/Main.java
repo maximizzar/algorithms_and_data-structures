9b4cebb8ba99ee957f7fe63d0eb3f7754c73dcd3
maximizzar <maximizzar@web.de>
2022-11-26 17:33:55 +0100

    main in beta

--- src/Main.java
+++ src/Main.java
@@ -1,62 +1,31 @@
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.logging.Logger;
 
 public class Main {
     public static void main(String[] args) {
-        InitTests initTests = new InitTests(8, 8192);
-        //SearchAlgo searchAlgo = new SearchAlgo();
-        SortAlgo sortAlgo = new SortAlgo();
-        Times times = new Times();
-        Scanner scanner = new Scanner(System.in);
+        Test test = new Test();
 
-        System.out.printf("if you want to change default type yes. Default: %s\n", initTests);
-        if(scanner.next().equalsIgnoreCase("yes")) {
-            System.out.println("enter repetitions");
-            initTests.setRepetitions(scanner.nextInt());
-            System.out.println("enter arraySize");
-            initTests.setArraySize(scanner.nextInt());
+        Integer length = 10, min = 1, max = 10;
+
+        for (int i = 0; i < 5; i++) {
+            if(i == 0)          length = 10;
+            else if (i == 1)    length = 100;
+            else if (i == 2)    length = 1000;
+            else if (i == 3)    length = 100000;
+            else                length = 100000000;
+
+            for (int j = 0; j < 5; j++) {
+                if(j == 0)          max = 10;
+                else if (j == 1)    max = 100;
+                else if (j == 2)    max = 1000;
+                else if (j == 3)    max = 100000;
+                else max = Integer.MAX_VALUE;
+
+                test.mergeSort(length,min,max);
+                test.randomizedQuicksort(length,min,max);
+
+            }
         }
-        scanner.close();
-
-        /*
-            Sort Tests
-         */
-
-        int[] arr = sortAlgo.countingSort(new Integer[] {9,9,9,9,9,9,8,7,6,0,5,4,3,0,2,1},9);
-
-        System.exit(0);
-
-        //bubbleSort
-        System.out.println("bubbleSort: ");
-
-        @SuppressWarnings("WrapperTypeMayBePrimitive")
-        Long bubbleSortDuration = 0L;
-
-        for (int i = 0; i < initTests.getRepetitions(); i++) {
-            initTests.setTestStart();
-            sortAlgo.bubbleSort(sortAlgo.randomIntegerArray(initTests.getArraySize()));
-            bubbleSortDuration += System.nanoTime() - initTests.getTestStart();
-        }
-        times.printTimes(times.getTimes(Math.toIntExact(bubbleSortDuration / initTests.getRepetitions())));
-
-
-        //insertionSort
-        System.out.println("insertionSort: ");
-
-        @SuppressWarnings("WrapperTypeMayBePrimitive")
-        Long insertionSortDurations = 0L;
-
-        for (int i = 0; i < initTests.getRepetitions(); i++) {
-            initTests.setTestStart();
-            sortAlgo.insertionSort(sortAlgo.randomIntegerArray(initTests.getArraySize()));
-            insertionSortDurations += System.nanoTime() - initTests.getTestStart();
-        }
-        times.printTimes(times.getTimes(Math.toIntExact(insertionSortDurations / initTests.getRepetitions())));
-
-        //mergeSort
-
-
-        //counting Sort
-
-
     }
 }
