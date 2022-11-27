0f4bbb36ee667689491c0477ee43a1163383492c
maximizzar <maximizzar@web.de>
2022-11-27 15:04:50 +0100

    test fix

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,27 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         Test test = new Test();
         Times times = new Times();
-        @SuppressWarnings("WrapperTypeMayBePrimitive")
-        Integer length = 10, min = 1, max = 10;
+        Integer length = 10, min = 1, max = 1000;
 
-
+        print(length,min,max,test,times);
+    }
+    public static void print(Integer length, Integer min, Integer max, Test test, Times times) {
         for (int i = 0; i < 5; i++) {
             if(i == 0)          length = 10;
             else if (i == 1)    length = 100;
             else if (i == 2)    length = 1000;
-            else if (i == 3)    length = 100000;
+            //else if (i == 3)    length = 100000;
             //else                length = 100000000;
 
             for (int j = 0; j < 5; j++) {
                 if(j == 0)          max = 10;
                 else if (j == 1)    max = 100;
                 else if (j == 2)    max = 1000;
-                else if (j == 3)    max = 100000;
-                else max = Integer.MAX_VALUE;
+                //else if (j == 3)    max = 100000;
+                //else max = Integer.MAX_VALUE - 1;
 
 
 
@@ -46,9 +49,8 @@ public class Main {
                         " ; " + times.toString(LUL3[2]) +
                         " ; " + times.toString(LUL4[2]));
                 System.out.println();
-                System.exit(0);
             }
-            System.out.println();
+            System.gc();
         }
     }
 }
